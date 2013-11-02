96518c9f2275aff0d31a7b8336eca9aa78f312f5
Jason Young <jason@ytechie.com>
2013-11-02 15:42:49 -0500

    Initial version for store

---
+++ YTechie/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace YTechie
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            Loaded += OnLoaded;
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            WebViewControl.NavigationStarting += WebViewControlOnNavigationStarting;
+            WebViewControl.NavigationCompleted += WebViewControlOnNavigationCompleted;
+        }
+
+        private void WebViewControlOnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            BusyIndicator.Visibility = Visibility.Collapsed;
+            BusyIndicator.IsActive = false;
+        }
+
+        private async void WebViewControlOnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            if (args.Uri.Host.EndsWith("ytechie.com", StringComparison.CurrentCultureIgnoreCase))
+            {
+                BusyIndicator.Visibility = Visibility.Visible;
+                BusyIndicator.IsActive = true;
+                return;
+            }
+
+            args.Cancel = true;
+            await Launcher.LaunchUriAsync(args.Uri, new LauncherOptions {DesiredRemainingView = ViewSizePreference.UseHalf});
+        }
+
+        private void RefreshButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            WebViewControl.Refresh();
+        }
+
+        private void HomeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            WebViewControl.Navigate(new Uri("http://www.ytechie.com"));
+        }
+    }
+}
